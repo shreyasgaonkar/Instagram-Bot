c5308588be77b232196e4aa892255d1d23b3b945
sggaonk <sggaonk@amazon.com>
2019-12-24 22:39:29 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+## Instagram Bot
+
+```app.py``` will let you scrap through all instagram posts containing a hashtag, and like them for you! Replace the username and password at ```user = InstagramBot('username', 'password')``` and run the function.
+
+### Pre-req:
+---
+1. ```$ pip install selenium```
+2. Gecko driver installed in python's directory (```$ which python``` to check the installed directory)
---
+++ app.py
@@ -0,0 +1,62 @@
+import re
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+class InstagramBot:
+    def __init__(self, username, password):
+        self.username = username,
+        self.password = password
+        self.bot = webdriver.Firefox()
+
+    def login(self):
+        bot = self.bot
+        bot.get("https://www.instagram.com/accounts/login/")
+        time.sleep(2)
+        email = bot.find_element_by_name("username")
+        password = bot.find_element_by_name("password")
+        email.clear()
+        password.clear()
+        email.send_keys(self.username)
+        password.send_keys(self.password)
+        password.send_keys(Keys.RETURN)
+        time.sleep(4)
+
+    def LikePhotos(self, hashtag):
+        bot = self.bot
+        bot.get("https://www.instagram.com/explore/tags/" + hashtag)
+        time.sleep(2)
+
+        # simulate scroll for lazy loading
+        for i in range(3):
+            bot.execute_script('window.scrollTo(0, document.body.scrollHeight)')
+            time.sleep(2)
+
+        # Get all links, which will be later cleaned for the posts
+        links = bot.find_elements_by_xpath('//div/a')
+        links_hrefs = []
+        for link in links:
+            # if links have href tag, add it to the list
+            try:
+                links_hrefs.append(link.get_attribute('href'))
+            except:
+                pass
+
+        for image in links_hrefs:
+            try:
+                # Check if the links contains posts
+                val = re.search(r'^https://www.instagram.com/p/', image)
+                bot.get(val.string)
+                time.sleep(2)
+                # Like post
+                bot.find_element_by_class_name('glyphsSpriteHeart__outline__24__grey_9').click()
+                time.sleep(1)
+            except:
+                pass
+
+
+# Magic!
+user = InstagramBot('username', 'password')
+user.login()
+user.LikePhotos('hashtag-to-search')
